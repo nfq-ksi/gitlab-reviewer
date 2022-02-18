90cdd701545be190ab3df9f9138ee926e5be4643
Zymantas Ponelis <zymantas.ponelis>
2022-02-18 10:18:26 +0200

    Added WIP Project Filter

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,22 @@ import Dashboard from 'pages/Dashboard';
 import NoMatch from 'pages/NoMatch';
 import Settings from 'pages/Settings';
 import Filters from './pages/Filters';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { GitLabApiProvider } from 'components/GitLabApiProvider';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-const client = new QueryClient();
-
 const App = (): JSX.Element => {
   return (
     <div className="App">
-      <GitLabApiProvider>
-        <QueryClientProvider client={client}>
-          <Router>
-            <Navbar />
-            <Switch>
-              <Route exact path="/" component={Dashboard} />
-              <Route path="/filters" component={Filters} />
-              <Route path="/settings" component={Settings} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Router>
-        </QueryClientProvider>
-      </GitLabApiProvider>
+      <Router>
+        <GitLabApiProvider>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/filters" component={Filters} />
+            <Route path="/settings" component={Settings} />
+            <Route component={NoMatch} />
+          </Switch>
+        </GitLabApiProvider>
+      </Router>
     </div>
   );
 };
